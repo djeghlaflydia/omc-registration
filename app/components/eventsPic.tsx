1044cd33517e9efad16a81a681a2a491cebe7768
DjeghlafLydia <djeghlaf.habiba@gmail.com>
2025-11-17 21:18:57 +0100

    .

---
+++ app/components/eventsPic.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const eventsPic = () => {
+  return (
+    <div>
+     <img 
+      src="events.png" 
+      alt="events"
+      className="mt-22 rotate-2 mb-12 "
+    />
+    </div>
+  )
+}
+
+export default eventsPic
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import Hero from "./components/hero";
 import FlipClock from "./components/flip-clock";
+import EventsPic from "./components/eventsPic";
 import Link from "next/link";
 
 export default function Home() {
@@ -11,13 +12,16 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center">
         <FlipClock />
       </div>
-      <Link href="/register">
-        <img
-          src="/button.svg"
-          alt="Wizard Illustration"
-          className="mx-auto mt-16 md:w-[18vw] hover:scale-105 transition-transform duration-300 cursor-pointer"
-        />
-      </Link>
+      <div className="flex flex-col items-center justify-center mt-8">
+      <Link href="/register" className="inline-block">
+      <img
+        src="/button.svg"
+        alt="Wizard Illustration"
+        className="mx-auto mt-16 md:w-[18vw] hover:scale-105 transition-transform duration-300 cursor-pointer"
+      />
+    </Link>
+    </div>
+      <EventsPic />
     </div>
   );
 }
