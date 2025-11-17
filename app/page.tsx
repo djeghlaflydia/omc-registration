3518cd446f0f067ff809c16989d9f71776f7af6b
aymen ghemam <gh.aymen39@gmail.com>
2025-11-17 22:56:26 +0100

    add the flip-timer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import Hero from "./components/hero";
+import FlipClock from "./components/flip-clock";
 
 export default function Home() {
   return (
     <div className="page-background">
       <Hero />
+      <div className="flex flex-col items-center justify-center py-16">
+        <FlipClock />
+      </div>
+
+      <div className="p-[50em]"></div>
     </div>
   );
 }
